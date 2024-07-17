ea8d55ceeec742486663982f6dcc2c616f6b9687
vinhddinh <vinh@dinh.cc>
2024-07-17 21:13:40 +1000

    more remaps

--- nvim/lua/vinhddinh/remap.lua
+++ nvim/lua/vinhddinh/remap.lua
@@ -5,8 +5,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "m", "<C-d>zz")
-vim.keymap.set("n", "M", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
@@ -37,3 +37,6 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader>]", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>[", vim.diagnostic.goto_prev)
